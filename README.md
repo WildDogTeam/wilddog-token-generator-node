846c2c83cd163d84f6096be98293902de358fa72
xiaoguangyu <xiaoguangyu@wilddog.com>
2016-04-04 16:28:49 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # wilddog-token-generator-node
 Json Web Token generator for Wilddog
+
+## 关于安全
+
+token generator 需要你的Wilddog超级密钥(secret)，所以你需要在收信人的服务器上生成token。安全起见，不要在任何客户端生成token。
+
+## 安装
+```
+
+```
+
+
+## 使用
+
+```js
+var WilddogTokenGenerator = require("wilddog-token-generator");
+var tokenGenerator = new WilddogTokenGenerator("<YOUR-WILDDOG-SECRET>");
+var token = tokenGenerator.createToken({ uid: "1", some: "arbitrary", data: "here" });
+
+```
+
+你传入createToken的属性能在规则表达式的`auth`变量中访问到。其他属性可选，但必须包含一个`uid`属性。
+
+## 认证
+
+Token在服务器生成后需要通知给客户端，由客户端通过Wilddog SDK中的API来执行认证操作。
+
+* JavaScript: `authWithCustomToken()`
+* Objective-C / Swift: `authWithCustomToken:withCompletionBlock:`
+* Java: `authWithCustomToken()`
+* REST: `auth`
---
+++ index.js
@@ -0,0 +1,49 @@
+var TOKEN_VERSION = 0;
+var jws = require('jws');
+var WilddogTokenGenerator = function (secret) {
+    this.secret = secret;
+
+}
+module.exports = WilddogTokenGenerator;
+WilddogTokenGenerator.prototype.createToken = function (data, opts) {
+    var claims = {}
+    var options = opts || {};
+    for (var o in options) {
+        switch (o) {
+            case 'expires':
+            case 'notBefore':
+                var code = (o === 'notBefore' ? 'nbf' : 'exp');
+                if (opts[o] instanceof Date) {
+                    claims[code] = Math.round(opts[o].getTime() / 1000);
+                } else {
+                    claims[code] = opts[o];
+                }
+                break;
+            case 'admin':
+                claims['admin'] = opts[o];
+                break;
+            case 'debug':
+                claims['debug'] = opts[o];
+                break;
+            case 'simulate':
+                claims['simulate'] = opts[o];
+                break;
+            case 'iat':
+                claims['iat'] = opts[o];
+                break;
+            default: {
+                throw new Error('createToken: unrecognized \"' + o + '\" option');
+            }
+        }
+    }
+    claims['v'] = TOKEN_VERSION;
+    claims['d'] = data;
+    return jws.sign({
+        header:{
+            typ:'JWT',
+            alg:'HS256'
+        },
+        payload:claims,
+        secret:this.secret
+    })
+}
